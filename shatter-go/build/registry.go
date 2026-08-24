53969ea2de0dc062de60592f881850072b184492
Test User <test@example.com>
2026-08-24 00:39:39 -0500

    str-35vtk.4: Support Windows registry locking

--- shatter-go/build/registry.go
+++ shatter-go/build/registry.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
-	"syscall"
 )
 
 const registryFileName = "binary_registry.json"
@@ -145,20 +144,4 @@ func registryStateDir(persistDir string) string {
 		filepath.Dir(persistDir),
 		"."+filepath.Base(persistDir)+registryStateDirectorySuffix,
 	)
-}
-
-func acquireRegistryPersistenceLock(stateDir string) (func(), error) {
-	lockPath := filepath.Join(stateDir, registryFileName+".lock")
-	lockFile, err := os.OpenFile(lockPath, os.O_CREATE|os.O_RDWR, 0o644)
-	if err != nil {
-		return nil, fmt.Errorf("open %q: %w", lockPath, err)
-	}
-	if err := syscall.Flock(int(lockFile.Fd()), syscall.LOCK_EX); err != nil {
-		_ = lockFile.Close()
-		return nil, fmt.Errorf("lock %q: %w", lockPath, err)
-	}
-	return func() {
-		_ = syscall.Flock(int(lockFile.Fd()), syscall.LOCK_UN)
-		_ = lockFile.Close()
-	}, nil
 }
